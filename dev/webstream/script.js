c8b1b4ef5042d7534964570a780aaada8591ec0a
Andrew McCann <andrewjmccann@gmail.com>
2020-12-27 16:36:32 -0500

    webstream

--- dev/webstream/script.js
+++ dev/webstream/script.js
@@ -1,7 +1,24 @@
-function update() {
-    var source = 'https://amcphd.com/dev/webstream/cam.jpeg',
-        timestamp = (new Date()).getTime(),
-        newUrl = source + '?_=' + timestamp;
-    document.getElementById("img").src = newUrl;
-    setTimeout(update, 1000);
+function refresh(node)
+{
+   var times = 3000; // gap in Milli Seconds;
+
+   (function startRefresh()
+   {
+      var address;
+      if(node.src.indexOf('?')>-1)
+       address = node.src.split('?')[0];
+      else 
+       address = node.src;
+      node.src = address+"?time="+new Date().getTime();
+
+      setTimeout(startRefresh,times);
+   })();
+
+}
+
+window.onload = function()
+{
+  var node = document.getElementById('img');
+  refresh(node);
+  // you can refresh as many images you want just repeat above steps
 }
